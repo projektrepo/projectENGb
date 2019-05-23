38947d68da3c0a19eea598a9e023be908a39f50b
projektrepo <projekt.repo1@gmail.com>
2019-05-24 00:21:12 +0200

    Feat: add flashcards activity

--- app/src/main/java/com/example/projecteng/learnlan.java
+++ app/src/main/java/com/example/projecteng/learnlan.java
@@ -6,11 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.projecteng.flashcard.FlashcardsActivity;
+
 public class learnlan extends AppCompatActivity {
 
 
     public Button przyciskczasy;
     public Button przyciskzadania;
+    public Button flashcardsButton;
+
     public void init()
     {
         przyciskczasy=(Button)findViewById(R.id.czasy);
@@ -22,6 +26,7 @@ public class learnlan extends AppCompatActivity {
                                          }
                                      }
         );
+
         przyciskzadania=(Button)findViewById(R.id.button4);
         przyciskzadania.setOnClickListener(new View.OnClickListener() {
                                          @Override
@@ -31,6 +36,15 @@ public class learnlan extends AppCompatActivity {
                                          }
                                      }
         );
+
+        flashcardsButton = (Button)findViewById(R.id.button5);
+        flashcardsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent flashcards = new Intent(learnlan.this, FlashcardsActivity.class);
+                startActivity(flashcards);
+            }
+        });
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
