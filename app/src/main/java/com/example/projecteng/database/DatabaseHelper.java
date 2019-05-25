6603476bce0e5a755dc17558dd8e669edead7fa3
projektrepo <projekt.repo1@gmail.com>
2019-05-25 20:46:46 +0200

    Feat: add database helper

---
+++ app/src/main/java/com/example/projecteng/database/DatabaseHelper.java
@@ -0,0 +1,30 @@
+package com.example.projecteng.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.example.projecteng.entity.Flashcard;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static String DATABASE_NAME = "english_learn.db";
+
+    public DatabaseHelper(Context context) {
+        super(context, "english_learn.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + Flashcard.TABLE_NAME + " (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "english VARCHAR(255), " +
+                "polish VARCHAR(255))");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + Flashcard.TABLE_NAME );
+    }
+}
